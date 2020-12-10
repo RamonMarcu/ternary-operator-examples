faf1bc6910c159a49c06e0d9311466704568eb8e
Ramon Marcu <ramondotmarcu@gmail.com>
2020-12-10 16:22:26 +0200

    Add files via upload

---
+++ refactoring if...else block to use a ternary operator.js
@@ -0,0 +1,110 @@
+// first example:
+
+
+let isLocked = false;
+
+if (isLocked) {
+  console.log('You will need a key to open the door.');
+} else {
+  console.log('You will not need a key to open the door.');
+}
+
+
+
+let isCorrect = true;
+
+if (isCorrect) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
+
+
+// Refactoring the first if...else block to use a ternary operator.
+
+// answer: 
+
+
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+
+let isCorrect = true;
+
+if (isCorrect) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
+
+// if run the answer, console displays: You will not need a key to open the door.
+Correct!
+I love that!
+
+// example no2: 
+
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+if (favoritePhrase === 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
+}
+
+// refactoring the if...else block to use ternary operator:
+
+// answer: 
+
+
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+(favoritePhrase === 'Love That!') ? console.log('I love that!') : console.log("I don't love that!");
+
+
+// if run the answer, console displays: You will not need a key to open the door.
+Correct!
+I love that!
+
+
+
+
